a1baa9c566edf4577c799633e8d311a46cc8b908
Lily Akhmetova <lilyakhmetovamail@gmail.com>
2020-02-17 01:01:13 +0300

    anime js

--- javascript/main.js
+++ javascript/main.js
@@ -28,6 +28,81 @@ let yellowRectangleAnimation = anime({
 let yellowRectangle = document.querySelector('.yellowrectangle')
 yellowRectangle.onclick = yellowRectangleAnimation.play
 
+let blueRectangleAnimation = anime({
+  targets: '.bluerectangle',
+  translateX: {
+    value: 250,
+    duration: 1000
+  },
+  translateY: {
+    value: -250,
+    duration: 1000
+  },
+  rotate: {
+    value: 360,
+    duration: 1800,
+    easing: 'easeInOutSine'
+  },
+  scale: {
+    value: 1.5,
+    duration: 1600,
+    delay: 800,
+    easing: 'easeInOutQuart'
+  },
+  delay: 100,
+  direction: 'alternate',
+  autoplay: false,
+  loop: true
+});
+
+let blueRectangle = document.querySelector('.bluerectangle')
+blueRectangle.onclick = blueRectangleAnimation.play
+
+let redRectangleAnimation = anime({
+  targets: '.redrectangle',
+  rotate: {
+    value: 360,
+    duration: 3000,
+    easing: 'easeInOutSine'
+  },
+  delay: 100,
+  direction: 'alternate',
+  autoplay: false,
+  loop: true
+})
+
+let redRectangle = document.querySelector('.redrectangle')
+redRectangle.onclick = redRectangleAnimation.play
+
+let pinkRectangleAnimation = anime({
+  targets: '.pinkrectangle',
+  rotate: {
+    value: -360,
+    duration: 5000,
+    easing: 'easeInOutSine'
+  },
+  delay: 100,
+  direction: 'alternate',
+  autoplay: false,
+  loop: true
+})
+
+let pinkRectangle = document.querySelector('.pinkrectangle')
+pinkRectangle.onclick = pinkRectangleAnimation.play
+
+let greenRectangleAnimation = anime({
+  targets: '.greenrectangle',
+  translateX: '-20vw',
+  direction: 'alternate',
+  loop: true,
+  autoplay: false,
+  easing: 'steps(5)'
+})
+
+let greenRectangle = document.querySelector('.greenrectangle')
+greenRectangle.onclick = greenRectangleAnimation.play
+
+
 let blackCircleAnimation = anime({
   targets: '.blackcircle',
   keyframes: [
